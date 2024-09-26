5c58065a2dc2c0218ace18493c826e43ee011504
felipeljj <fmartins.2001@alunos.utfpr.edu.br>
2024-09-26 10:08:07 -0300

    models

---
+++ WebAPI/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string? name { get; set; }
+        public string? email { get; set; }
+    }
+}
+ 
